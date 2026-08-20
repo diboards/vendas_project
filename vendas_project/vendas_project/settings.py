8cd528dfe818ab7c3a134ac1ecc6a1ac4e8bbfd3
diboards <155734323+diboards@users.noreply.github.com>
2026-08-20 15:50:23 -0300

    Update settings.py

--- vendas_project/vendas_project/settings.py
+++ vendas_project/vendas_project/settings.py
@@ -152,9 +152,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dionne.fernandes1@gmail.com'
-EMAIL_HOST_PASSWORD = 'wayyvwwucounwams'  # ← SENHA DE APP (sem espaços)
-DEFAULT_FROM_EMAIL = 'Mirna Boutique <dionne.fernandes1@gmail.com>'
+EMAIL_HOST_USER = 'mirnaboutique851@gmail.com'
+EMAIL_HOST_PASSWORD = 'cbszdezalairaizh'  # ← SENHA DE APP (sem espaços)
+DEFAULT_FROM_EMAIL = 'Mirna Boutique <mirnaboutique851@gmail.com>'
 PASSWORD_RESET_TIMEOUT = 3600
 
 
