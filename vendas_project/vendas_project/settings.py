d47ccfe79655bd792ca883a2d9692962260cc278
diboards <155734323+diboards@users.noreply.github.com>
2026-08-20 15:58:52 -0300

    Update settings.py

--- vendas_project/vendas_project/settings.py
+++ vendas_project/vendas_project/settings.py
@@ -146,11 +146,11 @@ LOGOUT_REDIRECT_URL = '/'
 PASSWORD_RESET_TIMEOUT = 3600  # 1 hora
 
 # Recuperação de senha
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_SSL = True
+EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mirnaboutique851@gmail.com'
 EMAIL_HOST_PASSWORD = 'cbszdezalairaizh'  # ← SENHA DE APP (sem espaços)
