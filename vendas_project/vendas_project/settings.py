45c1ce182fd939d88c4a8824db9029a0bb5bc8a0
diboards <155734323+diboards@users.noreply.github.com>
2026-08-25 15:20:58 -0300

    Update settings.py

--- vendas_project/vendas_project/settings.py
+++ vendas_project/vendas_project/settings.py
@@ -103,13 +103,13 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # importante no Render
-    'vendas.middleware.DynamicCacheMiddleware',  # 🔥 ADICIONE ESTE
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'vendas.middleware.DynamicCacheMiddleware',  # 🔥 ADICIONE ESTE
 ]
 
 ROOT_URLCONF = 'vendas_project.urls'
