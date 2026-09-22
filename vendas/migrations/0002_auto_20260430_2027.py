ca3eb6a7c9d6bb3a2e1e15ced670b4f4bac65766
diboards <155734323+diboards@users.noreply.github.com>
2026-09-22 11:54:40 -0300

    Update 0002_auto_20260430_2027.py

--- vendas/migrations/0002_auto_20260430_2027.py
+++ vendas/migrations/0002_auto_20260430_2027.py
@@ -9,11 +9,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='pedido',
-            name='status_pagamento',
-            field=models.CharField(max_length=20, default='pendente'),
-        ),
-        migrations.AddField(
-            model_name='pedido',
             name='status_entrega',
             field=models.CharField(max_length=20, default='aguardando'),
         ),
